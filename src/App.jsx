609dbc2b72169cc110dcecbc55cd63a8432bde6b
Sameerbaacha <sameeris6512@gmail.com>
2024-09-08 22:02:50 +0500

    add

---
+++ src/App.jsx
@@ -0,0 +1,86 @@
+
+import { useEffect, useState } from 'react'
+import './App.css'
+const factApi = "https://catfact.ninja/fact";
+const imgApi = "https://api.thecatapi.com/v1/images/search";
+
+function App() {
+  const [facts, setfacts] = useState({})
+  const [next, setnext] = useState(0)
+  const [catimg, setcatimg] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const [imgRes, factsRes] = await Promise.all(
+          [
+            fetch(imgApi),
+            fetch(factApi)
+          ]
+        );
+
+        const imgData = await imgRes.json();
+        const factsData = await factsRes.json();
+
+
+        setcatimg(imgData)
+        setfacts(factsData)
+        console.log(imgData)
+
+
+      }
+      catch (err) {
+        console.log("facts m error aya h", err.message)
+      }
+    }
+    fetchData();
+  }, [next])
+
+
+  return (
+    <div>
+      <div>
+        <h1>Cat Facts</h1>
+      </div>
+      <br />
+      <br />
+      <section className="text-gray-600 body-font">
+        <div
+          className="container mx-auto flex md:flex-row flex-col items-center"
+          bis_skin_checked={1}
+        >
+          <div
+            className=" md:w-96 w-5/6 "
+            bis_skin_checked={1}
+          >
+            <img
+              className="object-contain object-center rounded w-80"
+              alt="hero"
+              src={catimg[0]?.url}
+            />
+          </div>
+          <div
+            className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center"
+            bis_skin_checked={1}
+          >
+            <h6 className="hi title-font sm:text-2xl text-3xl mb-0 font-semibold text-gray-900 text-wrap text-justify">
+              {facts.fact}
+            </h6>
+
+            <div className="mt-8 flex justify-center" bis_skin_checked={1}>
+              <button onClick={() => { setnext(next + 1) }} className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                Button
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+
+
+
+
+export default App
